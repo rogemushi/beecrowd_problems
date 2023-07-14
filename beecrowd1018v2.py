0bcd3c453771ccc0f6ffbd803fdc7d96c0c2e5e0
rogemushi <rogervianagoncalves.alencar@gmail.com>
2023-07-14 16:57:49 -0300

    add iteração simplificada

--- beecrowd1018v2.py
+++ beecrowd1018v2.py
@@ -2,11 +2,9 @@ notas = (100, 50, 20, 10, 5, 2, 1)
 entrada = int(input())
 j = entrada
 
-if j != 0:
+while j != 0:
         for i in notas:
             
             print("{} nota(s) de R$ {},00".format(int(j/i), i))
             aux = (j%i)
             j = aux
-
-
