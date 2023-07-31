fb1104f47ef7e0584fa69fe45848a3acae99672a
rogemushi <rogervianagoncalves.alencar@gmail.com>
2023-07-31 00:43:14 -0300

    estudos de funções raízes

--- converter+decimal.py
+++ converter+decimal.py
@@ -1,8 +1,13 @@
-numero_digitado = int(input())
+dividendo = int(input())
+numero_digitado = dividendo
 quociente = 1
 lista = []
+sep = []
 while quociente >= 1:
     resto = numero_digitado % 2
     lista.insert(0, resto)
-    quociente = numero_digitado // 2
-    
+    quociente = dividendo // 2
+    dividendo = quociente
+
+binario = "".join(lista)
+print(binario)
